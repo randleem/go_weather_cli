bd79c55ee2641f98b340d2433c26aa149e74f2b6
randleem <erandle2211@gmail.com>
2025-10-31 16:48:54 +0000

    refactor to remove repetition

--- main.go
+++ main.go
@@ -26,17 +26,7 @@ func main() {
 
 	// GeoCoding: get address long, lat
 	geoCodingurl := "https://maps.googleapis.com/maps/api/geocode/json?address=birmingham,+GB&key=" + apiKey
-	geoCodeRes, err := http.Get(geoCodingurl)
-	if err != nil {
-		fmt.Println("Error: weatherAPI")
-	}
-	defer geoCodeRes.Body.Close()
-	if geoCodeRes.StatusCode != http.StatusOK {
-		log.Fatalf("status code error: %d %s", geoCodeRes.StatusCode, geoCodeRes.Status)
-	}
-	fmt.Println(geoCodeRes.Status)
-
-	geoCode, err := parseResponse(geoCodeRes.Body)
+	geoCode, err := getAPI(geoCodingurl)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
@@ -46,7 +36,29 @@ func main() {
 	fmt.Println("Address:", geoCode.Results[0].FormattedAddress)
 
 	// Weather: get weather data
-	resp, err := http.Get(fmt.Sprintf("https://weather.googleapis.com/v1/currentConditions:lookup?key=%v&location.latitude=%v&location.longitude=%v", apiKey, lat, long))
+	weatherAPI := fmt.Sprintf("https://weather.googleapis.com/v1/currentConditions:lookup?key=%v&location.latitude=%v&location.longitude=%v", apiKey, lat, long)
+	weather, err := getAPI(weatherAPI)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	parseWeather(geoCode.Results[0].FormattedAddress, weather.WeatherResponse)
+}
+
+func parseResponse(r io.Reader) (GoogleResponse, error) {
+	dec := json.NewDecoder(r)
+	var response GoogleResponse
+	if err := dec.Decode(&response); err != nil {
+		return response, fmt.Errorf("failed to decode response: %w", err)
+	}
+	return response, nil
+}
+
+func parseWeather(address string, weather google.WeatherResponse) {
+	fmt.Printf("The weather today at %v is looking %v, with a temperaure of %v and a %v%% chance of rain,", address, weather.WeatherCondition.Type, weather.Temperature.Degrees, weather.Precipitation.Probability.Percent)
+}
+
+func getAPI(api string) (GoogleResponse, error) {
+	resp, err := http.Get(api)
 	if err != nil {
 		fmt.Println("Error: weatherAPI")
 	}
@@ -55,18 +67,5 @@ func main() {
 	if resp.StatusCode != http.StatusOK {
 		log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
 	}
-	weather, err := parseResponse(resp.Body)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	fmt.Printf("The weather today at %v is looking %v, with a temperaure of %v and a %v%% chance of rain,", geoCode.Results[0].FormattedAddress, weather.WeatherCondition.Type, weather.Temperature.Degrees, weather.Precipitation.Probability.Percent)
-}
-
-func parseResponse[T GoogleResponse](r io.Reader) (T, error) {
-	dec := json.NewDecoder(r)
-	var response T
-	if err := dec.Decode(&response); err != nil {
-		return response, fmt.Errorf("failed to decode response: %w", err)
-	}
-	return response, nil
+	return parseResponse(resp.Body)
 }
